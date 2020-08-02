a28d3f6efa9577bd85cfd02800771c5c15e835b5
Demilola Adefokun <d.adefokun@outlook.com>
2020-08-02 14:43:48 +0300

    created tabsScreen and favoritesScreen. closes #2

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('DeliMeal'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Categories', icon: Icon(Icons.category)),
+                Tab(text: 'Favorites', icon: Icon(Icons.favorite)),
+              ],
+            ),
+          ),
+          body: TabBarView(children: [
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ])),
+    );
+  }
+}
