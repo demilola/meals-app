568a75915f4a3c1a99948cb09330249e6b698507
Demilola Adefokun <d.adefokun@outlook.com>
2020-08-30 16:04:25 +0300

    CategoryMealsScreen accepts availableMeals instead

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   static const String id = 'CategoryMealsScreen';
+  final List<Meal> availableMeals;
+
+  const CategoryMealsScreen(this.availableMeals);
 
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
@@ -16,11 +18,11 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   bool loadedInitData = false;
 
   @override
-  void initState() { 
+  void initState() {
     //..
     super.initState();
-    
   }
+
 //had to use didChangeDependencies because ModalRoute.of(context) uses inherited widget
   @override
   void didChangeDependencies() {
@@ -30,7 +32,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           ModalRoute.of(context).settings.arguments as Map<String, String>;
       categoryTitle = routeArguments['categoryTitle'];
       String categoryId = routeArguments['categoryID'];
-      displayedMeals = DUMMY_MEALS
+      displayedMeals = widget.availableMeals
           .where((meal) => meal.categories.contains(categoryId))
           .toList();
       loadedInitData = true;
