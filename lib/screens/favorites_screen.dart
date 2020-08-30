42d1a3e66ca96c9fee875a1a6cb4a7d83945980a
Demilola Adefokun <d.adefokun@outlook.com>
2020-08-30 19:47:19 +0300

    basic UI for displaying favorites

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({Key key}) : super(key: key);
+  final List<Meal> favoriteMeals;
+  const FavoritesScreen(this.favoriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      
-    );
+    return favoriteMeals.isEmpty
+        ? Center(child: Text('Nothing to see yet'))
+        : ListView.builder(
+              itemCount: favoriteMeals.length,
+              itemBuilder: (ctx, index) => MealItem(
+                meal: favoriteMeals[index],
+              ),
+            );
   }
 }
